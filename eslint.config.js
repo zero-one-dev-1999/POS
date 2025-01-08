18980133b026aefdd657bf2357eee17a2a832b6a
Tursunboy Soyibjonov <soyibjonovtursunboy.zero.one@gmail.com>
2025-01-08 17:33:09 +0500

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+	{ ignores: ['dist', 'node_modules', 'public'] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettier,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'prettier/prettier': [0, {}, { usePrettierrc: true }],
+			'no-unused-vars': 'warn',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/no-explicit-any': 'warn',
+		},
+	},
+)
